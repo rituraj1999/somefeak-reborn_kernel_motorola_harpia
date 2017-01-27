7eb5259946bc840d166b5ce6341267c81404e595
root <facumo.fm@gmail.com>
2017-01-27 02:06:00 -0300

    Fix script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget http://download1337.mediafire.com/7st1l15yrsbg/4nwy86xivvbsib7/arm-cortex_a7-linux-gnueabihf-linaro_4.9.3-2015.03-build_2015_03_15.tar.xz
-tar -xvf arm-cortex_a7-linux-gnueabihf-linaro_4.9.3-2015.03-build_2015_03_15.tar.xz
+tar -xf arm-cortex_a7-linux-gnueabihf-linaro_4.9.3-2015.03-build_2015_03_15.tar.xz
 ARCH=arm
 # CROSS_COMPILE=./linaro/bin/arm-eabi-
 CROSS_COMPILE=./arm-cortex_a7-linux-gnueabihf-linaro_4.9.4-2015.06/bin/arm-eabi-
