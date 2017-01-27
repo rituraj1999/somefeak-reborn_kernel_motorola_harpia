6b5e56ad3a5bb0a1974c3704cbbda9adfce2e48c
root <facumo.fm@gmail.com>
2017-01-27 18:15:57 -0300

    Script

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# wget "https://github.com/FacuM/android_kernel_motorola_msm8916/blob/squid_nougat/arm-cortex_a7-linux-gnueabihf-linaro_4.9.4-2015.06-build_2015_07_15.tar.xz?raw=true" -o ~/arm-cortex_a7-linux-gnueabihf-linaro_4.9.4-2015.06-build_2015_07_15.tar.xz
-tar -xvf /home/travis/build/FacuM/android_kernel_motorola_msm8916/arm-cortex_a7-linux-gnueabihf-linaro_4.9.3-2015.03-build_2015_03_15.tar.xz
+wget "https://github.com/FacuM/android_kernel_motorola_msm8916/blob/squid_nougat/arm-cortex_a7-linux-gnueabihf-linaro_4.9.4-2015.06-build_2015_07_15.tar.xz?raw=true"
+mv "arm-cortex_a7-linux-gnueabihf-linaro_4.9.4-2015.06-build_2015_07_15.tar.xz?raw=true" "arm-cortex_a7-linux-gnueabihf-linaro_4.9.3-2015.03-build_2015_03_15.tar.xz"
+tar -xvf arm-cortex_a7-linux-gnueabihf-linaro_4.9.3-2015.03-build_2015_03_15.tar.xz
 make ARCH=arm CROSS_COMPILE=./arm-cortex_a7-linux-gnueabihf-linaro_4.9.4-2015.06/bin/arm-eabi- -j4 all
