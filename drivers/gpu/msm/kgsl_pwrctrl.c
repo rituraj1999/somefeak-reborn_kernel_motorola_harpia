a6dbc36f0afb6b90868b364b2d25731e2ae20f09
fluxi <linflux@arcor.de>
2017-03-07 03:32:38 -0600

    msm: kgsl: Report correct GPU frequency in sysfs
    Change-Id: I1aac90d0554b9de0511ffb78042177a5a23855ce
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -353,6 +353,7 @@ static ssize_t kgsl_pwrctrl_thermal_pwrlevel_store(struct device *dev,
 					 struct device_attribute *attr,
 					 const char *buf, size_t count)
 {
+	unsigned long freq;
 	struct kgsl_device *device = kgsl_device_from_dev(dev);
 	struct kgsl_pwrctrl *pwr;
 	int ret;
