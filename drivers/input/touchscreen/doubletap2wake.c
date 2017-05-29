2510dcc4b649041c437d90ac0f450a1821b9ab02
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-05-29 16:23:27 -0500

    dt2w: fixed halfscreen mode
    this code need update! for more updates check my git!

--- drivers/input/touchscreen/doubletap2wake.c
+++ drivers/input/touchscreen/doubletap2wake.c
@@ -1,7 +1,8 @@
 /*
  * drivers/input/touchscreen/doubletap2wake.c
  *
- * Copyright (C) 2016 Carlos Arriaga <carlosarriagadv@gmail.com> (osprey,surnia,merlin-port dt2w)
+ * Copyright (C) 2016 Carlos Arriaga <carlosarriagacm@gmail.com> (surnia,merlin and harpia porting)
+ * Copyright (C) 2015 engstk <eng.stk@sapo.pt> (osprey port, fixes and changes)
  * Copyright (c) 2013, Dennis Rassmann <showp1984@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -68,7 +69,7 @@ static struct work_struct dt2w_input_work;
 static int __init read_dt2w_cmdline(char *dt2w)
 {
 	if (strcmp(dt2w, "1") == 0) {
-		pr_info("[cmdline_dt2w]: DoubleTap2Wake enabled. | dt2w='%s'\n", dt2w);
+		pr_info("[cmdline_dt2w]: DoubleTap2Wake halfscreen enabled. | dt2w='%s'\n", dt2w);
 		dt2w_switch = 1;
 	} else if (strcmp(dt2w, "2") == 0) {
 		pr_info("[cmdline_dt2w]: DoubleTap2Wake fullscreen enabled. | dt2w='%s'\n", dt2w);
@@ -138,11 +139,8 @@ static void detect_doubletap2wake(int x, int y, bool st)
         pr_info(LOGTAG"x,y(%4d,%4d) single:%s\n",
                 x, y, (single_touch) ? "true" : "false");
 #endif
-	if (x < 100 || x > 980)
-        	return;
-
-	if (dt2w_switch < 2 && y < 1000)
-        	return;
+	if (dt2w_switch < 2 && y < 480)
+		return;
 
 	if ((single_touch) && (dt2w_switch > 0) && (exec_count) && (touch_cnt)) {
 		touch_cnt = false;
