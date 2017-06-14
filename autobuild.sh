6b29c15908d5e3dee4bbf4b94d42000260ea9a90
FacuM <facumo.fm@gmail.com>
2017-06-14 20:16:10 -0300

    general: auto build script, added interactive mode if variables aren't defined at runtime.

--- autobuild.sh
+++ autobuild.sh
@@ -4,19 +4,33 @@
 # ===================================== #
 # Facundo Montero (facumo.fm@gmail.com) #
 # ===================================== #
+# Variable definition
+DEVCODENAME="$1"
 # Function definition
 function buildkernel {
  printf '\nClean the build environment.\n'
- make -j8 clean
+# make -j8 clean
  printf "\nBegin building...\n"
  make -j8 all
- bash build_cwm_zip.sh $1
+ bash build_cwm_zip.sh $DEVCODENAME
+}
+function yesno {
+ printf '\nWould you like to set it up right now? (type Y for 'yes', or N or everything else for 'no'): '
+ read YESNO
+ if [ "$YESNO" == 'Y' ]
+ then
+  :
+ else
+  exit 1
+ fi
 }
 # Check if user wrote device name.
 if [ -z "$1" ]
 then
  printf '\nYou must type your device codename before beginning.\n\n  i.e.: bash autobuild.sh harpia\n'
- exit 1
+ yesno
+ printf '\nPlease type your device codename: '
+ read DEVCODENAME
 else
  printf "\nDevice codename: $1\n"
 fi
@@ -24,7 +38,9 @@ fi
 if [ -z "$ARCH" ]
 then
  printf '\nVariable $ARCH is not defined. Define it and try again.\n\n  i.e.: ARCH='arm'\n'
- exit 1
+ yesno
+ printf '\nPlease type the architecture: '
+ read ARCH
 else
  printf "\nArchitecture: $ARCH\n"
 fi
@@ -32,7 +48,9 @@ fi
 if [ -z "$CROSS_COMPILE" ]
 then
  printf '\n$CROSS_COMPILE is not defined. Define it and try again.\n\n  i.e.: CROSS_COMPILE='~/toolchain/bin/arm-eabi-'\n'
- exit 1
+ yesno
+ printf '\nPlease type the toolchain path: '
+ read CROSS_COMPILE
 else
  printf "\nToolchain path: $CROSS_COMPILE\n"
 fi
@@ -43,7 +61,7 @@ then
  buildkernel
 else
  printf "\n.config file not existing. Loading defaults...\n"
- make -j8 $1_defconfig
+ make -j8 $DEVCODENAME_defconfig
  if [ "$?" -ne 0 ]
  then
   exit 1
