c0643698c961e05f93fa39ff6652d01b4143757f
FacuM <facumo.fm@gmail.com>
2017-06-21 23:09:20 -0300

    build.sh: and now?

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 git clone https://bitbucket.org/UBERTC/arm-eabi-4.9.git
 # mv "arm-cortex_a7-linux-gnueabihf-linaro_4.9.4-2015.06-build_2015_07_15.tar.xz?raw=true" "arm-cortex_a7-linux-gnueabihf-linaro_4.9.3-2015.03-build_2015_03_15.tar.xz"
 # tar -xvf arm-cortex_a7-linux-gnueabihf-linaro_4.9.3-2015.03-build_2015_03_15.tar.xz
-make ARCH='arm' CROSS_COMPILE='./arm-eabi-4.9/bin/arm-eabi-' -j8 all
+make ARCH='arm' CROSS_COMPILE='arm-eabi-4.9/bin/arm-eabi-' -j8 all
 # bash build_cwm_zip.sh
